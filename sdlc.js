62e6b0942cdebba6cea9978babe87c2786c1682e
Corbin Broadhead <poujmail@gmail.com>
2023-01-19 20:31:32 -0700

    SDLC done.

--- sdlc.js
+++ sdlc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
-// Implementation
-// Design
-// Maintain
-// Analysis of Requirements
-// Testing & Integration
 // Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+var analysisOfRequirements = "This is the second step, in which you will determine the requirements of the software you will develop. This is where you will define the user requirements identified in the previous step."
+var design = "This is the third step, in which you will actually develop the new software. This is where the machine's parts are built, if you will."
+var implementation = "This is the fourth step, where you will assemble the machine by implementing the newly constructed parts."
+var testingAndIntegration = "This is the fifth step, where the whole machine is tested and integrated into it's environment in order to see it's functionality."
+var maintain = "This is the final step, where the machine's maintenance will take place. It is important to keep the machine well oiled and to patch anything that needs to be fixed."
